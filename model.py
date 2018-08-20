7a2df1375be383221b155e770ab32e7321152c28
Jed <jed.isom@cyient-insights.com>
2018-08-20 09:50:12 -0700

    Adding project files to Git repository to be uploaded to GitHub

---
+++ model.py
@@ -0,0 +1,35 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class QNetwork(nn.Module):
+    """Actor (Policy) Model."""
+
+    def __init__(self, state_size, action_size, seed, fc1_units=64, fc2_units=64, fc3_units=None):
+        """Initialize parameters and build model.
+        Params
+        ======
+            state_size (int): Dimension of each state
+            action_size (int): Dimension of each action
+            seed (int): Random seed
+            fc1_units (int): Number of nodes in first hidden layer
+            fc2_units (int): Number of nodes in second hidden layer
+        """
+        super(QNetwork, self).__init__()
+        self.seed = torch.manual_seed(seed)
+        self.fc1 = nn.Linear(state_size, fc1_units)
+        self.fc2 = nn.Linear(fc1_units, fc2_units)
+        if fc3_units is not None:
+            self.fc3 = nn.Linear(fc2_units, fc3_units)
+            self.output = nn.Linear(fc3_units, action_size)
+        else:
+            self.fc3 = None
+            self.output = nn.Linear(fc2_units, action_size)
+
+    def forward(self, state):
+        """Build a network that maps state -> action values."""
+        x = F.relu(self.fc1(state))
+        x = F.relu(self.fc2(x))
+        if self.fc3 is not None:
+            x = F.relu(self.fc3(x))
+        return self.output(x)
